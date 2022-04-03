56159353bafddfc0d6c3e65846d846ba24723004
Sergio <se.diaz60@gmail.com>
2022-04-02 20:18:18 -0500

    [Update]: first part project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/app/header/header.component';
 import { QuoteModule } from './quote/quote.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
